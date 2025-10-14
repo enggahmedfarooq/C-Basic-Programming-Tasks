4f3f54199230bc3def66b4ff78fe2064337d3859
Ahmed Farooq <ahmedfarooqgcp@gmail.com>
2025-10-15 00:20:07 +0500

    Task2_AbsoluteValue.c

---
+++ Task2_AbsoluteValue.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        num = -num;
+
+    printf("Absolute value: %d\n", num);
+    return 0;
+}
