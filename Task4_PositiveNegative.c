32a75db8a5b33c90627238f9927b02004120615b
Ahmed Farooq <ahmedfarooqgcp@gmail.com>
2025-10-15 00:21:14 +0500

    Task4_PositiveNegative.c

---
+++ Task4_PositiveNegative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num > 0)
+        printf("Number is positive.\n");
+    else if (num < 0)
+        printf("Number is negative.\n");
+    else
+        printf("Number is zero.\n");
+
+    return 0;
+}
